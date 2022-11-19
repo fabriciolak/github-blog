079d1b8dcb3aef16c983cf7fd2f755702306e262
fabricio <fabriciospfullstack@gmail.com>
2022-11-19 14:36:13 -0300

    feat: :sparkles: implemented validation form

--- src/pages/Blog/components/SearchForm/index.tsx
+++ src/pages/Blog/components/SearchForm/index.tsx
@@ -1,18 +1,58 @@
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { useSearchParams } from 'react-router-dom'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+
 import {
   SearchFormContainer,
   SearchFormHeader,
   SearchFormInput,
 } from './styles'
 
-export function SearchForm() {
+const searchInputSchema = z.object({
+  query: z.string(),
+})
+
+type SearchInputType = z.infer<typeof searchInputSchema>
+
+interface SearchFormProps {
+  comments: number
+}
+
+export function SearchForm({ comments }: SearchFormProps) {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const searchQuery = searchParams.get('search') || ''
+
+  const { register, watch } = useForm<SearchInputType>({
+    resolver: zodResolver(searchInputSchema),
+    defaultValues: {
+      query: searchQuery,
+    },
+  })
+
+  const search = watch('query')
+
+  useEffect(() => {
+    setSearchParams({ search })
+  }, [setSearchParams, search])
+
   return (
     <SearchFormContainer>
       <SearchFormHeader>
         <h2>Publicações</h2>
-        <p>6 publicações</p>
+        <p>
+          {comments} {comments > 2 ? 'publicações' : 'publicação'}
+        </p>
       </SearchFormHeader>
 
-      <SearchFormInput type="text" placeholder="Buscar conteúdo" />
+      <SearchFormInput
+        type="text"
+        placeholder="Buscar conteúdo"
+        {...register('query')}
+      />
     </SearchFormContainer>
   )
 }
